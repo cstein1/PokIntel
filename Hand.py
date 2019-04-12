9b0b773e6896b347168a9e7064b9a3b4dee6a916
cstein1 <cstein1@trinity.edu>
2019-04-12 19:38:57 -0400

    Added Hand drawing and printing capabilities

---
+++ Hand.py
@@ -0,0 +1,17 @@
+
+class Hand:
+    def __init__(self, deck, playername = ""):
+        self.cards = deck.draw(5)
+        self.name = playername
+
+    def draw(self, deck, num_cards = 1):
+        self.cards.append(deck.draw(num_cards))
+
+    def __str__(self):
+        ostr = "{0} contains the following cards:\n".format(self.name if self.name else "Hand")
+        for card in self.cards:
+            ostr += "\t{0}".format(str(card))
+        return ostr
+
+#    @property
+#    def matr(self):
