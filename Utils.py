99024d0dd9e2cd1b83b92a48e57a4aac9a7cb6c2
cstein1 <cstein1@trinity.edu>
2019-04-13 17:09:27 -0400

    Implementing win cons: Flushes, Full House, Straight

--- Utils.py
+++ Utils.py
@@ -1,3 +1,6 @@
 def window(iterator, window_width):
     inds = [[a for a in range(i-window_width,i)] for i in range(window_width, len(iterator)+1)]
     return [tuple([iterator[i] for i in ind]) for ind in inds]
+
+def last_occ(itr, itm):
+  return ''.join(str(i) for i in itr).rindex(str(itm))
--- WinCons.py
+++ WinCons.py
@@ -1,11 +1,12 @@
 
-from Utils import window
+from Utils import window, last_occ
 
 class WinCon:
     def __init__(self, hand1):
         if not isinstance(hand1, list):
             raise Exception("WinCons expects a vector representation")
         # splitting into 4 lists for each suit
+        self.flat = hand1[:]
         newlist = []
         for i in range(4):
           newlist.append(hand1[:13])
@@ -14,7 +15,9 @@ class WinCon:
         self.h = newlist
 
     def id(self):
-        # In order of which is best
+        # In (reverse_indexed) order of which is best
+        # If wincon is true, it will return the highest relevant card,
+        # O/w it will return -1 and try the next wincon
         wincons = [
             self._RoyalFlush,
             self._StraightFlush,
@@ -27,17 +30,18 @@ class WinCon:
             self._Pair,
             self._HighCard
         ]
-        # TODO: next_highest_card =
-        for wincon in wincons:
-            if wincon:
-                return wincon#, highest_card_involved
+        for ind, wincon in enumerate(wincons):
+            win_ind = wincon()
+            if win_ind >= 0:
+                # First element should be largest num
+                return (len(wincons)-ind)*51 + highest_relevant_card
 
     def _RoyalFlush(self):
         # if any of the suits in your hand have 1s for each of the highest 5 cards
-        flush = False
-        for suit in self.h:
-            flush |= all(suit[-5:])
-        return flush
+        for ind, suit in enumerate(self.h):
+            if all(suit[-5:]):
+                return last_occ(self.flat, 1)
+        return -1
 
     def _StraightFlush(self):
         # if any of the suits have 5 consecutive cards
@@ -46,13 +50,49 @@ class WinCon:
             slider = window(suit, 5)
             for consec in slider:
                 if all(consec):
-                    return True
-        return False
+                    return last_occ(self.flat, 1)
+        return -1
 
     def _Quads(self):
-        quad = False
+        # Four of the same card number
         for ind in range(len(self.h[0])):
+            quad = True
             for suit in self.h:
-                if all(suit[ind]):
-                    return True
-        return False
+                quad &= suit[ind]
+            if quad:
+                return last_occ(self.flat, 1)
+        return -1
+
+    def _FullHouse(self):
+        # One pair, One Trip
+        pass;
+
+    def _Flush(self):
+        # Four of a suit
+        for suit in self.h:
+            if suit.count(1) == 5:
+                return last_occ(self.flat, 1)
+        return -1
+
+    def _Straight(self):
+        # Five Numerically adjacent cards, suits are irrelevant
+        for ind, card in enumerate(self.flat):
+            if card:
+                # Ace can only be highest (ind%13==12) or lowest (ind%13==8)
+                if ind%13 <= 8:
+                    itr = range(ind,ind+5)
+                elif ind%13==12:
+                    itr = range(ind-12,ind-7)
+                else:
+                    continue;
+                straight = True
+                for i in itr:
+                    straight &= any(
+                        list(map(lambda suit: suit[i], self.h))
+                        )
+                if straight:
+                    return last_occ(self.flat, 1)
+        return -1
+
+    def _Trips(self):
+        
